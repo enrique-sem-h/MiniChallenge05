90e204ccd1150e3132973641a3443a3339a396ae
vhparaujo <vhugopacheco59@gmail.com>
2024-05-20 14:11:24 -0300

    Core data manager created

--- MiniChallange05/MiniChallange05 Watch App/Model/UserDataManager.swift
+++ MiniChallange05/MiniChallange05 Watch App/Model/UserDataManager.swift
@@ -6,3 +6,83 @@
 //
 
 import Foundation
+import CoreData
+
+class UserDataManager: ObservableObject {
+    
+    static let shared = UserDataManager()
+    
+    let container = NSPersistentContainer(name: "UserData")
+    
+    var user: User?
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    // MARK: - Basics operations
+    func fetchUser() {
+        let request = NSFetchRequest<User>(entityName: "User")
+        
+        do{
+            if let fetchedUser = try container.viewContext.fetch(request).first {
+                user = fetchedUser
+            }
+        } catch {
+            fatalError("Problema na requisição de um usuario")
+        }
+    }
+    
+    func saveData(){
+        do {
+            try container.viewContext.save()
+            fetchUser()
+        } catch {
+            fatalError("Erro na criação de um usuario")
+        }
+    }
+    
+    func createUser(tipoCigarro: String, cigarrosPorDia: Int16) {
+        let user: User = User(context: container.viewContext)
+        user.id = UUID()
+        user.tipoCigarro = tipoCigarro
+        user.cigarrosPorDia = cigarrosPorDia
+        
+        saveData()
+    }
+    
+//    func createUser(tipoCigarro: String, cigarrosPorDia: Int16, macosNoCigarro: Int16, custoMaco: Double, quandoParou: Date, startStreak: Date) {
+//        let user: User = User(context: context)
+//        user.id = UUID()
+//        user.tipoCigarro = tipoCigarro
+//        user.cigarrosPorDia = cigarrosPorDia
+//        user.macosNoCigarro = macosNoCigarro
+//        user.custoMaco = custoMaco
+//        user.quandoParou = quandoParou
+//        user.startStreak = startStreak
+//        
+//        saveData()
+//    }
+    
+    func editUser(tipoCigarro: String, cigarrosPorDia: Int16, macosNoCigarro: Int16, custoMaco: Double, quandoParou: Date, startStreak: Date, user: User) {
+        user.id = UUID()
+        user.tipoCigarro = tipoCigarro
+        user.cigarrosPorDia = cigarrosPorDia
+        user.macosNoCigarro = macosNoCigarro
+        user.custoMaco = custoMaco
+        user.quandoParou = quandoParou
+        user.startStreak = startStreak
+        
+        saveData()
+    }
+    
+//    func deleteUserInfo(user: User){
+//        context.delete(user)
+//        saveData()
+//    }
+    
+}
--- MiniChallange05/MiniChallange05 Watch App/Views/Onboarding/Onboarding1View.swift
+++ MiniChallange05/MiniChallange05 Watch App/Views/Onboarding/Onboarding1View.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct Onboarding1View: View {
+
+    @Binding var estados: AllViews
+    
+    @State var cigarrosPorDia: Int = 0
+    @State var tipoCigarro = ""
+    
+    @State var possibilidadeDeCigarros: [Int16] = [0,1,2,3,4,5]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Quantos cigarros por dia você costuma fumar?")
+            
+            Picker("", selection: $cigarrosPorDia) {
+                ForEach(0..<5) {
+                    Text("\(possibilidadeDeCigarros[$0])")
+                }
+            }.padding(.horizontal)
+            //.frame(width: 90, height: 100)
+            
+            Button(action: {
+                UserDataManager.shared.createUser(tipoCigarro: "convencional", cigarrosPorDia: Int16(cigarrosPorDia))
+                
+                estados = .onboarding2
+            }, label: {
+                Text("Proxima")
+            })
+            
+        }
     }
 }
 
 #Preview {
-    Onboarding1View()
+    Onboarding1View(estados: .constant(.onboarding1))
 }
