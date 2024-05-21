8d0de1e45c95b9462a061cb255cf69913d9f9f4d
kauamiguel <kauamcm@gmail.com>
2024-05-21 16:41:19 -0300

    Adding onboardingView2

--- MiniChallange05/MiniChallange05 Watch App/Views/Onboarding/Components/SmokingTypeView.swift
+++ MiniChallange05/MiniChallange05 Watch App/Views/Onboarding/Components/SmokingTypeView.swift
@@ -6,19 +6,60 @@
 //
 
 import SwiftUI
+import WatchKit
 
 struct SmokingTypeView: View {
     @Environment(PageManager.self) var pageManager
+    let width = WKInterfaceDevice.current().screenBounds.width
+    let heigth = WKInterfaceDevice.current().screenBounds.height
     
     var body: some View {
         VStack {
-            Text("SmokingType")
-                .padding()
             
-            Button("CigaretteCount") {
-                pageManager.page = .cigaretteCount
+            Text("O que você fuma atualmente?")
+                .font(.headline)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .center)
+                .padding(.bottom, 10)
+                .minimumScaleFactor(0.7)
+            
+            //Container with both buttons and texts
+            HStack{
+                
+                //Container with one button and one text
+                VStack{
+                    Button(action: {
+                        pageManager.page = .cigaretteCount
+                    }, label: {
+                        Rectangle()
+                            .frame(width: width / 2.2)
+                            .cornerRadius(8.0)
+                    })
+                    .buttonStyle(PlainButtonStyle())
+                    
+                    Text("Cigarro Convencional")
+                        .font(.system(size: 12))
+                        .multilineTextAlignment(.center)
+                        .minimumScaleFactor(0.7)
+                }
+                
+                VStack{
+                    Button(action: {
+                        pageManager.page = .cigaretteCount
+                    }, label: {
+                        Rectangle()
+                            .frame(width: width / 2.2)
+                            .cornerRadius(8.0)
+                    })
+                    .buttonStyle(PlainButtonStyle())
+                    
+                    Text("Cigarro eletronico")
+                        .multilineTextAlignment(.center)
+                        .font(.system(size: 12))
+                        .minimumScaleFactor(0.7)
+                }
             }
-            .padding()
+            
+            
         }
     }
 }
