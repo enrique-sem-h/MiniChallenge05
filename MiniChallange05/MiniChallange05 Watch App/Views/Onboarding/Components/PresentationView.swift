d4e3f00ee365e8110771cbd8cbc899c3a2ea401d
Giovanni Favorin de Melo <Giovannifavorim@gmail.com>
2024-05-20 21:33:49 -0300

    não sei de onde veio esse codigo

--- MiniChallange05/MiniChallange05 Watch App/Views/Onboarding/Components/PresentationView.swift
+++ MiniChallange05/MiniChallange05 Watch App/Views/Onboarding/Components/PresentationView.swift
@@ -12,13 +12,32 @@ struct PresentationView: View {
     
     var body: some View {
         VStack {
-            Text("Presentation")
-                .padding()
             
-            Button("SmokingYpe") {
+            Text("Apresentação do App")
+                .font(.title3)
+                .bold()
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                .padding(.bottom, 10)
+                .minimumScaleFactor(0.7)
+            
+            
+            
+            
+            Text("Para começar, precisamos que insira algumas inforamções a seguir.")
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                .font(.footnote)
+                .padding(.bottom, 10)
+                .minimumScaleFactor(0.5)
+            
+            
+            Button("Continuar") {
                 pageManager.page = .smokingType
             }
-            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+            .frame(maxWidth: .infinity, alignment: .center)
+            
         }
     }
 }
