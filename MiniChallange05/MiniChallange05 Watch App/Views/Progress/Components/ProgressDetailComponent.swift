cdb13b6e28a4d8f038ac5313f8fe19f647e7dd2d
vhparaujo <vhugopacheco59@gmail.com>
2024-05-27 17:16:07 -0300

    Layout HealthProgressDetail

--- MiniChallange05/MiniChallange05 Watch App/Views/Progress/Components/ProgressDetailComponent.swift
+++ MiniChallange05/MiniChallange05 Watch App/Views/Progress/Components/ProgressDetailComponent.swift
@@ -8,10 +8,50 @@
 import SwiftUI
 
 struct ProgressDetailComponent: View {
+    @State var percentage: Float = 0.8
+    @State var healthType: String = "Oxigenação no sangue"
+    @State var aboutText: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut."
+    
+    @State var sourceText: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    
     var body: some View {
-        VStack {
+        
+        ScrollView {
             
-        }
+            HStack {
+                Image(systemName: "heart.fill")
+                    .imageScale(.large)
+                Spacer()
+                let p = Int(percentage * 100)
+                Text("\(Int(p))%")
+            }
+            .padding()
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            
+            Text(healthType)
+                .font(.title2)
+            
+            HStack {
+                Text("About")
+                    .font(.title3)
+                Spacer()
+            }
+            .padding(.top)
+            
+            Text(aboutText)
+            
+            HStack {
+                Text("Source")
+                    .font(.title3)
+                Spacer()
+            }
+            .padding(.top)
+            
+            Text(sourceText)
+        
+        }.scenePadding(.horizontal)
+        
     }
 }
 
