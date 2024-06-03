f55d0b53d7c9727c55bf106d0d9b5830b53de7cd
Giovanni Favorin de Melo <Giovannifavorim@gmail.com>
2024-06-03 15:47:53 -0300

    stepper

--- MiniChallange05/MiniChallange05 Watch App/Views/Onboarding/Components/PackCostView.swift
+++ MiniChallange05/MiniChallange05 Watch App/Views/Onboarding/Components/PackCostView.swift
@@ -19,22 +19,75 @@ struct PackCostView: View {
         ScrollView {
             VStack {
                 Text("Quanto custa o maço de cigarro?")
-                    .padding(.bottom, 5)
-                    .minimumScaleFactor(0.5)
-                    .frame(width: screenWidth * 0.9, height: screenHeight * 0.18)
+                    .font(.title2)
+                    .minimumScaleFactor(0.7)
+                    .frame(maxWidth: .infinity, maxHeight: 100, alignment: .leading)
                 
-                HStack(alignment: .center){
-                    Text(Locale.current.currencySymbol ?? "R$")
-                    OnboardingPicker(selectedNumber: $tempVar)
-                }.frame(width: screenWidth * 0.4, height: screenHeight * 0.3)
-                    .padding(.bottom,10)
+                HStack {
+                    Button {
+                        if tempVar > 0 {
+                            tempVar -= 1
+                        }
+                    } label: {
+                        Image(systemName: "minus")
+                            .resizable()
+                            .scaledToFit()
+                    }
+                    .clipShape(Circle())
+                    
+                    OnboardingPicker(selectedNumber: $tempVar, range: 0..<201)
+                        .frame(width: 90, height: 85)
+                        .padding(.bottom,10)
+                    
+                    Button {
+                        if tempVar < 200 {
+                            tempVar += 1
+                        }
+                        
+                    } label: {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .scaledToFit()
+                    }
+                    .clipShape(Circle())
+                    
+                }
+                
+                Text("\(Locale.current.currencySymbol ?? "R$")\(tempVar) em média por maço")
+                
+                Button {
+                    if tempVar >= 190 {
+                        tempVar = 200
+                    } else {
+                        tempVar += 10
+                    }
+                } label: {
+                    Text("Add $10")
+                }
+                
+                Button {
+                    if tempVar >= 100 {
+                        tempVar = 200
+                    } else {
+                        tempVar += 100
+                    }
+                } label: {
+                    Text("Add $100")
+                }
+                .padding(.bottom, 14)
                 
                 GenericBackAndNextButton(fowardView: .smokingHours, backwardsView: .cigarettesPerPack, tempVar: $tempVar, defVar: $defVar)
-                
+                    .padding(.top)
                 
             }
+            .padding(.horizontal)
         }.onDisappear{
             userPreferences.smokeCost = Double(defVar)
         }
     }
+}
+
+#Preview {
+    PackCostView(tempVar: 2, defVar: .constant(200), viewAtual: .constant(.cigarettesPerPack), userPreferences: UserPreferences())
+        .environment(PageManager())
 }
