6544b7f61496dee5febf47f286a2404e9b8f466c
kauamiguel <kauamcm@gmail.com>
2024-06-04 16:42:09 -0300

    Adicionando a funcao de libear uma streak com base no tempo

--- MiniChallange05/MiniChallange05 Watch App/Model/AchievementModel.swift
+++ MiniChallange05/MiniChallange05 Watch App/Model/AchievementModel.swift
@@ -27,5 +27,46 @@ class AchievementModel{
 extension AchievementModel{
     func releaseArchievment(){
         
+        let daysElapsed = getStreakDays()
+        
+        switch daysElapsed{
+        case AchievementCases.startJourney.rawValue:
+            //Inicio da jornada
+            break
+        case AchievementCases.firstDay.rawValue:
+            //Adicionar o UUID da streak de um dia no array do coreData
+            break
+        case AchievementCases.firstWeek.rawValue:
+            //Adicionar o UUID da streak de uma semana no array do coreData
+            break
+        case AchievementCases.halfMonth.rawValue:
+            //Adicionar o UUID da streak de 15 dias no array do coreData
+            break
+        case AchievementCases.month.rawValue: 
+            //Adicionar o UUID da streak de um mês no array do coreData
+            break
+        case AchievementCases.threeMonths.rawValue:
+            //Adicionar o UUID da streak de três meses no array do coreData
+            break
+        case AchievementCases.halfYear.rawValue:
+            //Adicionar o UUID da streak de 6 meses no array do coreData
+            break
+        case AchievementCases.year.rawValue:
+            //Adicionar o UUID da streak de um ano no array do coreData
+            break
+        default:
+            print("Non case was executed")
+            break
+        }
+        
     }
+    
+    //Get the current period in the streak to check wich achievement should be released
+    func getStreakDays() -> Int?{
+        guard let startStreak = DataManager.shared.userEntity?.startStreak else {return 0}
+        let calendar = Calendar.current
+        let daysElapsed =  calendar.dateComponents([.day], from: startStreak, to: .now).day
+        return daysElapsed
+    }
+    
 }
