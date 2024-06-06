6cda1db6af2d519c5b2b229a7db07e2023b7a01f
vhparaujo <vhugopacheco59@gmail.com>
2024-06-05 23:43:32 -0300

    complication adicionada

---
+++ MiniChallange05/widgetAppleWatch/widgetAppleWatch.swift
@@ -0,0 +1,105 @@
+//
+//  WidgetAppleWatch.swift
+//  WidgetAppleWatch
+//
+//  Created by Victor Hugo Pacheco Araujo on 05/06/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), icon: "widget")
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), icon: "widget")
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, icon: "widget")
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let icon: String
+}
+
+struct WidgetAppleWatchEntryView : View {
+    var entry: Provider.Entry
+    
+    @Environment(\.widgetFamily) var family
+
+    var body: some View {
+        
+        ZStack {
+            AccessoryWidgetBackground()
+            
+            switch family {
+            case .accessoryCorner:
+                Image("widget")
+                    .resizable()
+                    .scaledToFit()
+           
+            case .accessoryCircular:
+                Image("widget")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+            default:
+                Image("widget")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+            }
+        }
+        
+    }
+}
+
+@main
+struct WidgetAppleWatch: Widget {
+    let kind: String = "WidgetAppleWatch"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(watchOS 10.0, *) {
+                WidgetAppleWatchEntryView(entry: entry)
+                    .containerBackground(.fill.tertiary, for: .widget)
+            } else {
+                WidgetAppleWatchEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .configurationDisplayName("NicQuit!")
+        .description("You can access the app here.")
+        .supportedFamilies([.accessoryCircular, .accessoryCorner])
+    }
+}
+
+#Preview(as: .accessoryCorner) {
+    WidgetAppleWatch()
+} timeline: {
+    SimpleEntry(date: .now, icon: "widget")
+}
+
+#Preview(as: .accessoryCircular) {
+    WidgetAppleWatch()
+} timeline: {
+    SimpleEntry(date: .now, icon: "widget")
+}
